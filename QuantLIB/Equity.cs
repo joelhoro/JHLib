e42bf67319299e7b386c1446c191dcefadc06d5b
Joel Horowitz <joel@horowitz.com>
2014-05-15 00:26:55 +0100

    First attempt for equity multi model

--- QuantLIB/Equity.cs
+++ QuantLIB/Equity.cs
@@ -98,4 +98,86 @@ namespace JHLib.QuantLIB
                 return pathsCache[i];
         }
     }
+
+    public class EquityMultiModel
+    {
+        public List<Equity> equities;
+        public DenseMatrix correlations;
+        public List<Date> diffusiondates;
+        public int N;
+
+        private List<DenseVector> pathsCache;
+
+        private DenseMatrix _cholesky;
+
+        public void Initialize()
+        {
+            var size = equities.Count;
+            _cholesky = new DenseMatrix(size, size);
+            double rho = correlations[0, 1];
+            _cholesky[0, 0] = 1;
+            _cholesky[0, 1] = 0;
+            _cholesky[1, 0] = rho;
+            _cholesky[1, 1] = Math.Sqrt(1 - rho * rho);
+        }
+
+        private DenseMatrix Variances()
+        {
+            var variances = new DenseMatrix(diffusiondates.Count, equities.Count);
+
+            for (int i = 0; i < equities.Count; i++)
+            {
+                double previousVariance = 0;
+                for (int j = 0; j < diffusiondates.Count;j++ )
+                {
+                    Date date = diffusiondates[j];
+                    double variance = equities[i].Variance(date);
+                    variances[j,i] = variance - previousVariance;
+                    previousVariance = variance;
+                }
+            }
+            return variances;
+        }
+
+        //private IEnumerable<DenseVector> LogReturns(Normal normal)
+        //{
+        //    var variances = Variances();
+
+        //    foreach (var variance in variances)
+        //    {
+        //        var stdev = Math.Sqrt(variance);
+        //        var drift = -0.5 * variance;
+        //        var returns = new DenseVector(N);
+        //        for (int i = 0; i < N; i++)
+        //            returns[i] = Math.Exp(normal.Sample() * stdev + drift);
+        //        returns /= returns.Average();
+        //        yield return returns;
+        //    }
+        //}
+
+        //private IEnumerable<DenseVector> Spots(Normal normal)
+        //{
+        //    var spots = DenseVector.Create(N, (_) => equities.spot);
+        //    foreach (var returns in LogReturns(normal))
+        //    {
+        //        spots = spots.PointwiseMultiply(returns) as DenseVector;
+        //        yield return spots;
+        //    }
+        //}
+
+        //public void ComputePaths(Normal normal)
+        //{
+        //    pathsCache = Spots(normal).ToList();
+        //}
+
+        //public DenseVector PathsOnDate(Date date)
+        //{
+        //    int i = diffusiondates.IndexOf(date);
+        //    if (i == -1)
+        //        throw new Exception("Can't find " + date + " in diffusion dates");
+        //    else
+        //        return pathsCache[i];
+        //}
+    }
+
 }
