3272e15fa7e905eea84ecf101d0e282096406ad0
Joel <joel@horowitz.com>
2015-07-14 22:58:46 -0400

    Improving test coverage

--- QuantLIB/Core/Dates.cs
+++ QuantLIB/Core/Dates.cs
@@ -8,6 +8,9 @@ namespace JHLib.QuantLIB.Core
         public double frequency;
         public Frequency(double frequency) { this.frequency = frequency; }
         public double ToDays() { return frequency;  }
+        public static Frequency d1 { get { return new Frequency(1); } }
+        public static Frequency d2 { get { return new Frequency(2); } }
+        public static Frequency w { get { return new Frequency(7); } }
     }
 
     public class Date
@@ -16,7 +19,7 @@ namespace JHLib.QuantLIB.Core
         public Date(DateTime dateTime) { _datetime = dateTime.Date;  }
 
         public static Date Today { get { return new Date(DateTime.Now); } }
-
+        public static Date Tomorrow { get { return Today + Frequency.d1; } }
         public static IEnumerable<Date> GenerateSchedule(Date startDate, Frequency frequency, int numberOfDates )
         {
             Date date = startDate;
@@ -30,6 +33,11 @@ namespace JHLib.QuantLIB.Core
         public static Date operator +(Date startDate, Frequency frequency)
         {
             return new Date(startDate._datetime.AddDays(frequency.ToDays()));
+        }
+
+        public static Date operator -(Date startDate, Frequency frequency)
+        {
+            return new Date(startDate._datetime.AddDays(-frequency.ToDays()));
         }
 
         public static TimeSpan operator -(Date date1, Date date2)
@@ -49,6 +57,11 @@ namespace JHLib.QuantLIB.Core
         public override string ToString()
         {
             return _datetime.ToShortDateString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return (obj is Date) && (_datetime == (obj as Date)._datetime);
         }
     }
 }
---
+++ Tests/Date.cs
@@ -0,0 +1,34 @@
+﻿using JHLib.QuantLIB.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+
+    [TestClass]
+    public class DateTests
+    {
+        [TestMethod]
+        public void TestComparison()
+        {
+            Assert.IsTrue(Date.Tomorrow > Date.Today);
+            Assert.IsFalse(Date.Tomorrow < Date.Today);
+        }
+
+        [TestMethod]
+        public void TestOperators()
+        {
+            Assert.AreEqual((Date.Today + Frequency.d1) - Frequency.d1, Date.Today);
+            Assert.AreEqual((Date.Today - Frequency.d1) + Frequency.d1, Date.Today);
+            Assert.AreEqual(Date.Tomorrow - Date.Today, TimeSpan.FromDays(1));
+            Assert.AreEqual(Date.Today + Frequency.d2 - Date.Today, TimeSpan.FromDays(2));
+            Assert.AreEqual(Date.Today + Frequency.w - Date.Today, TimeSpan.FromDays(7));
+            Assert.AreNotEqual(Date.Today, 0);
+            Assert.AreEqual("7/14/2015",new Date(new DateTime(2015,7,14)).ToString());
+        }
+    }
+}
