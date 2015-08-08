d21030bb1f7ad45e20c741bfbd247e4ff5ed2f3f
Joel <joel@horowitz.com>
2015-08-08 11:56:09 -0400

    Interface / test for huffman data

---
+++ Tests/CompressionTests.cs
@@ -0,0 +1,31 @@
+﻿using JHLib.Utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass]
+    public class CompressionTests
+    {
+        [TestMethod]
+        public void HuffmanTest()
+        {
+            var message = @"
+That's the witness all right,
+the one from the Barksdale case.
+Gant, William. 41 years.
+Single headshot, close range.
+Bullet pancaked on the inner skull.
+-Ain't necessarily what it looks like.
+-No?
+";
+            var huffmanData = HuffmanData.FromString(message);
+
+            Assert.AreEqual(message, huffmanData.ToString());
+        }
+    }
+}
---
+++ Utils/HuffmanData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JHLib.Utils
+{
+    public class HuffmanData
+    {
+        public static HuffmanData FromString(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ToString()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
